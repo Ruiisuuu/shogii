cac4fed69317c66ef964b6b86d91cc208a2c8df9
Ruiisuuu <40916893+Ruiisuuu@users.noreply.github.com>
2018-07-08 00:55:14 +0200

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -54,8 +54,8 @@ function setup() {
 	let sideButton = createButton("I sexually identify as a color");
 	sideButton.mousePressed(() => flipBoard());
 	
-	// https
-	var socket = io.connect('https://shogi2-shogiii.1d35.starter-us-east-1.openshiftapps.com:8000');
+	// http
+	var socket = io.connect('http://shogi2-shogiii.1d35.starter-us-east-1.openshiftapps.com:8000/', {'forceNew':true });
 	
 	console.log("WORK DAMNIT");
   	// We make a named event called 'mouse' and write an
