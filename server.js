ab130305b31efb56db7ffa4dd16a9379d587aa85
Ruiisuuu <40916893+Ruiisuuu@users.noreply.github.com>
2018-07-07 16:01:47 +0200

    openshift server

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ var app = express();
 
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 3000, listen);
-
-// This call back just tells us that the server has started
-function listen() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://' + host + ':' + port);
-}
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 app.use(express.static('public'));
 
